018c489d222d85295babaa1c7f225dec8e18e61b
Fred Uggla <Fred.Uggla@gmail.com>
2016-07-17 20:09:25 +0200

    tabs all around

--- dot.lua
+++ dot.lua
@@ -1,73 +1,73 @@
 #!/usr/bin/luajit
 local dot = {
-    version 		= 'dot 0.2',
-    description		= 'Handles configuration files',
-    usage          	= 'dot <action> <target>\n\taction:\t "import" from system or "deploy" to system.\n\ttarget:\t Name of specific configuration or "all."',
-    url            	= 'github.com/fledo/dots',
-    license        	= [[
-        Copyright (C) 2016  Fred Uggla
+	version			= 'dot 0.2',
+	description		= 'Handles configuration files',
+	usage			= 'dot <action> <target>\n\taction:\t "import" from system or "deploy" to system.\n\ttarget:\t Name of specific configuration or "all."',
+	url				= 'github.com/fledo/dots',
+	license			= [[
+		Copyright (C) 2016	Fred Uggla
 
-        This program is free software; you can redistribute it and/or modify
-        it under the terms of the GNU General Public License as published by
-        the Free Software Foundation; either version 2 of the License, or
-        (at your option) any later version.
+		This program is free software; you can redistribute it and/or modify
+		it under the terms of the GNU General Public License as published by
+		the Free Software Foundation; either version 2 of the License, or
+		(at your option) any later version.
 
-        This program is distributed in the hope that it will be useful,
-        but WITHOUT ANY WARRANTY; without even the implied warranty of
-        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-        GNU General Public License for more details.
+		This program is distributed in the hope that it will be useful,
+		but WITHOUT ANY WARRANTY; without even the implied warranty of
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		GNU General Public License for more details.
 
-        You should have received a copy of the GNU General Public License along
-        with this program; if not, write to the Free Software Foundation, Inc.,
-        51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+		You should have received a copy of the GNU General Public License along
+		with this program; if not, write to the Free Software Foundation, Inc.,
+		51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-    ]]
+	]]
 }
 local index = require('index')
 
 function dot.deploy(file)
 	if file == 'all' then
 		for k,v in pairs(index) do
-            dot.deploy(k)
-        end	
+			dot.deploy(k)
+		end 
 	elseif index[file] then
 		for k,v in pairs(index[file]) do
-            copy(v[2], v[1])
-        end	
+			copy(v[2], v[1])
+		end 
 	else
 		print(file .. ' dot not found in index.')
 	end
 end
 
 function dot.import(file)
-    if file == 'all' then
-        for k,v in pairs(index) do
-            dot.import(k)
-        end     
-    elseif index[file] then
-        for k,v in pairs(index[file]) do
-            copy(v[1], v[2])
-        end     
-    else
-        print(file .. ' dot not found in index.')
-    end
+	if file == 'all' then
+		for k,v in pairs(index) do
+			dot.import(k)
+		end		
+	elseif index[file] then
+		for k,v in pairs(index[file]) do
+			copy(v[1], v[2])
+		end		
+	else
+		print(file .. ' dot not found in index.')
+	end
 end
 
 function copy(from, to, force)
-    if force == 'yes' then
-	    os.execute('cp -fv $HOME' .. from .. ' $HOME' .. to)
-    else
-	    os.execute('cp -fiv $HOME' .. from .. ' $HOME' .. to)
-    end 
+	if force == 'yes' then
+		os.execute('cp -fv $HOME' .. from .. ' $HOME' .. to)
+	else
+		os.execute('cp -fiv $HOME' .. from .. ' $HOME' .. to)
+	end 
 end
 
 -- ## argument handler
 -- call arg[1] if it's a function
 -- echo arg[1] if it's a string
 if type(dot[arg[1]]) == 'function' and arg[2] ~= nil then
-    dot[arg[1]](arg[2])
+	dot[arg[1]](arg[2])
 elseif type(dot[arg[1]]) == 'string' then
-    print(dot[arg[1]])
+	print(dot[arg[1]])
 else
-    print(dot.version .. ", " .. dot.description .. "\n" .. dot.usage .. "\n" .. dot.url)
+	print(dot.version .. ", " .. dot.description .. "\n" .. dot.usage .. "\n" .. dot.url)
 end
--- index.lua
+++ index.lua
@@ -1,24 +1,24 @@
 local index = {
-    bash = {
-        {"/.bashrc", "/dots/bash/rc"},
-        {"/.bash_profile", "/dots/bash/profile"},
-        {"/.bash_aliases", "/dots/bash/aliases"},
-    }, 
-    i3 = {
-	    {"/.config/i3/config", "/dots/i3/config"} 
-    },
-    vim = {
-        {"/.vimrc", "/dots/vim/vimrc"}
-    },
-    tmux = {
-        {"/.config/tmux/config", "/dots/tmux/config"}
-    },
-    x = {
-        {"/.Xresources", "/dots/x/resources"}
-    },
-    redshift = {
-        {"/.config/redshift.conf", "/dots/redshift/config"}
-    }
-}   
+	bash = {
+		{"/.bashrc", "/dots/bash/rc"},
+		{"/.bash_profile", "/dots/bash/profile"},
+		{"/.bash_aliases", "/dots/bash/aliases"},
+	},
+	i3 = {
+		{"/.config/i3/config", "/dots/i3/config"}
+	},
+	vim = {
+		{"/.vimrc", "/dots/vim/vimrc"}
+	},
+	tmux = {
+		{"/.config/tmux/config", "/dots/tmux/config"}
+	},
+	x = {
+		{"/.Xresources", "/dots/x/resources"}
+	},
+	redshift = {
+		{"/.config/redshift.conf", "/dots/redshift/config"}
+	}
+}	
 
 return index 
